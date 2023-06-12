f4d1e2c0d6bdbef25663bfaa0bdf5d884666f595
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-06-12 13:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 function prompt(){
 let input = prompt("Enter a String", "");
 	if(input != null){
-    let output = alert(a)
-	
+		console.log(alert(input));
+	}
+}
+function alert(){
+const n = input.length();
+	const count = 0;
+for(let i = 0;i<n;i++){
+if(input[i]==input[i+1]){
+	count++;
+}
+}
+	if(count<2){
+ alert(input[i]);
 	}
 }
