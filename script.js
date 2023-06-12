8ab72e54139ca0d20b3d4826c60de64eed058fbf
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-06-12 13:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-//your JS code here. If required.
-function prompt(){
-let input = prompt("Enter a String", "");
-	if(input != null){
-		console.log(alert(input));
-	}
+//your JS code here. If required
+	function prompt() {
+  const input = prompt("Enter a string:");
+  let charCount = {};
+
+  // Count the occurrences of each character
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first non-repeated character
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    if (charCount[char] === 1) {
+      alert("The first non-repeated character is: " + char);
+      return char;
+    }
+  }
+
+  alert("There are no non-repeated characters in the input string.");
+  return null;
 }
-function alert(){
-const n = input.length();
-	const count = 0;
-for(let i = 0;i<n;i++){
-if(input[i]==input[i+1]){
-	count++;
-}
-}
-	if(count<2){
- alert(input[i]);
-	}
+
+prompt();
+
 }
